8afb85750743723b7fd9399778861b94252f90c1
ChampaGadito <johncamacho117@gmail.com>
2025-05-19 10:25:11 +0800

    Create QuizzappQuestions.js

---
+++ Quizzapp/QuizzappQuestions.js
@@ -0,0 +1,137 @@
+const quizData = {
+  subjen: [
+    {
+      question: "What is the past tense of the verb GO?",
+      options: ["Go", "Goes", "Went", "Gosn't"],
+      answer: "Went"
+    },
+    {
+      question: "What is a noun?",
+      options: ["A word that describes an action", "A word that shows emotion", "A word that names a person, place, thing, or idea", "A word that joins sentences"],
+      answer: "A word that names a person, place, thing, or idea"
+    },
+    {
+      question: "Which word is an adjective?",
+      options: ["Run" , "Quickly" , "Happy" , "Jump"],
+      answer: "Happy"
+    },
+    {
+      question: "Choose the correct sentence using the word Cat?",
+      options: [" The cat sleep on the sofa." , "Cat on sofa the is." , "The cat is sleeping on the sofa." , "Sofa is sleeping the cat."],
+      answer: "The cat is sleeping on the sofa."
+    },
+    {
+      question: "What punctuation mark do we use at the end of a question?",
+      options: ["Period" , "Question Mark" , "Comma" , "Exlamation Mark"],
+      answer: "Question Mark"
+    }
+  ],
+  web: [
+    {
+      question: "What does HTML stand for?",
+      options: ["Hyper Text Markup Language", "HighText Machine Language", "Hyperlink and Text Markup Language", "How to make Lumpia"],
+      answer: "Hyper Text Markup Language"
+    },
+    {
+      question: "What does CSS stand for?",
+      options: ["Computer Style Sheets", "Cascading Style Sheets", "Creative Style Syntax", "Colorful Style Sheets"],
+      answer: "Cascading Style Sheets"
+    },
+    {
+      question: "Which HTML tag is used for the largest heading?",
+      options: ["<h2o>", "<h6>", "<h1>" , "</h1>"],
+      answer: "<h1>"
+    },
+    {
+      question: "What does HTTP stand for?",
+      options: ["Hyperlink Transfer Transmission", "High Text Transfer Protocol", "Hyper Text Transfer Protocol", " Hyperlink Text Transmission Protocol"],
+      answer: "Hyper Text Transfer Protocol"
+    },
+    {
+      question: "Which HTML tag is used to include a JavaScript file?",
+      options: ["<js>", "<script>", "<source>" , "<link>"],
+      answer: "<script>"
+    },
+  ],
+  subjsc: [
+    {
+      question: "Which part of the plant makes food?",
+      options: ["Root" , "Stem" , "Leaf" , "Flower"],
+      answer: "Leaf"
+    },
+    {
+      question: "What do humans need to breathe?",
+      options: ["Water" , "Food" , "Air" , "Rugby"],
+      answer: "Air"
+    },
+    {
+      question: "Which of these is a solid??",
+      options: ["Milk" , "Juice" , "Rock" , "Steam"],
+      answer: "Rock"
+    },
+    {
+      question: "What is the main source of energy for Earth?",
+      options: ["Moon" , "Fire" , "Sun", "Wind"],
+      answer: "Sun"
+    },
+    {
+      question: "Which sense organ do we use to see?",
+      options: ["Ears" , "Eyes" , "Nose" , "Tongue"],
+      answer: "Eyes"
+    },
+  ],
+  subjfi: [
+    {
+      question: "Ano ang tawag sa sagisag o salitang may kahulugang hindi tuwiran o literal?",
+      options: ["Tayutay" , "Talinhaga" , "Bugtong" , "Sawikain"],
+      answer: "Sawikain"
+    },
+    {
+      question: "Sino ang tinaguriang ANG AMA NG PANULAANG TAGALOG?",
+      options: ["Jose Rizal" , "Francisco Balagtas" , "Lope K. Santos" , "Jose Manalo"],
+      answer: "Francisco Balagtas"
+    },
+    {
+      question: "Alin sa mga sumusunod ang isang pang-abay?",
+      options: ["Maganda" , "Mabilis" , "Malungkot" , "Masaya"],
+      answer: "Mabilis"
+    },
+    {
+      question: "Ano ang gamit ng pang-ugnay sa pangungusap na: MAHUSAY SIYANG UMAWIT KAYA NANALO SIYA SA PATIMPALAK",
+      options: ["Paninsay" , "Pananhi" , "Panapos" ,"Panlinaw"],
+      answer: "Pananhi"
+    },
+    {
+      question: "Alin sa mga sumusunod ang halimbawa ng tambalang salita?",
+      options: ["Bahaghari" , "Malakas" , "Magada" , "Bahay"],
+      answer: "Bahaghari"
+    },
+  ],
+  subjma: [
+    {
+      question: "What is the value of 8 × 7?",
+      options: ["54" , "56" , "64" , "58"],
+      answer: "56"
+    },
+    {
+      question: "What is the square root of 81?",
+      options: ["7" , "8" , "9" , "10"],
+      answer: "9"
+    },
+    {
+      question: "If 5x = 25, what is the value of x?",
+      options: ["3" , "4" , "5" , "6"],
+      answer: "5"
+    },
+    {
+      question: "What is the next number in the sequence: 2, 4, 8, 16, __?",
+      options: ["20" , "24" , "30" , "32"],
+      answer: "32"
+    },
+    {
+      question: "What is 15% of 200?",
+      options: ["25" , "30" , "35" , "40"],
+      answer: "30"
+    },
+  ] 
+};
